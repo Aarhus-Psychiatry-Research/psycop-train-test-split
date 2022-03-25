3f91264d4b6a2353418bd64e3f87244072370d18
Martin Bernstorff <ryqiem@gmail.com>
2022-03-25 14:39:57 +0100

    Moving towards MVP

--- src/application/main.py
+++ src/application/main.py
@@ -1,18 +1,31 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from loaders import sql_load
+from psycoptts.add_outcomes import add_outcome_from_csv
+
+def load_all_patients():
+    view = "[FOR_kohorte_indhold_pt_journal_inkl_2021_feb2022]"
+    sql = "SELECT * FROM [fct]." + view 
+
+    df = sql_load(sql, database="USR_PS_FORSK", chunksize = None)
+    return df["dw_ek_borger"]
 
 if __name__ == "__main__":
     stratify_cols = ["outcome1", "outcome2"]
     random_state = 42
 
+    all_patient_ids = load_all_patients().to_frame()
+
+    out_df = add_outcome_from_csv(all_patient_ids, "outcome_ids/lung_cancer_ids.csv", "lung_cancer")
+
     # TODO
     # Generate in_df from the csvs using add_outcome_from_csv()
 
     X_train, X_intermediate = train_test_split(
-        in_df["dw_ek_borger"],
+        out_df["dw_ek_borger"],
         test_size=0.4,
         random_state=random_state,
-        stratify=in_df[stratify_cols],
+        stratify=all_patient_ids[stratify_cols],
     )
 
     X_test, X_val = train_test_split(
--- src/psycoptts/add_outcomes.py
+++ src/psycoptts/add_outcomes.py
@@ -7,5 +7,5 @@ def add_outcome_from_df(df_in, df_outcome, new_col_name, id_colname="dw_ek_borge
     return pd.merge(df_in, df_outcome, how="left", on=id_colname).fillna(0)
 
 
-def add_outcome_from_csv(path):
-    return add_outcome_from_df(pd.read_csv(path))
+def add_outcome_from_csv(df_in, df_outcome_path, new_colname):
+    return add_outcome_from_df(df_in, pd.read_csv(df_outcome_path), new_colname, id_colname="dw_ek_borger")
