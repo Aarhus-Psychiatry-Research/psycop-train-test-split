ad8483faf4534306f7ae19a75a8087fce399f424
Martin Bernstorff <ryqiem@gmail.com>
2022-03-25 15:45:33 +0100

    Remove redundant imports

--- src/application/main.py
+++ src/application/main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from loaders import sql_load
 from psycoptts.add_outcomes import add_outcome_from_csv
 
 from sqlalchemy import create_engine
